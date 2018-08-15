d399b307a9efb0688d9352c32efa954546bb1582
Ybrin <koray@koska.at>
2018-08-15 14:09:21 +0200

    Finish network selection in settings

--- app/src/main/java/com/boilertalk/ballet/settings/SettingsFragment.java
+++ app/src/main/java/com/boilertalk/ballet/settings/SettingsFragment.java
@@ -3,18 +3,23 @@ package com.boilertalk.ballet.settings;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.boilertalk.ballet.R;
+import com.boilertalk.ballet.database.RPCUrl;
+import com.boilertalk.ballet.toolbox.VariableHolder;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class SettingsFragment extends Fragment {
+
+    @BindView(R.id.settings_select_network_subtitle) TextView selectedNetwork;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -32,22 +37,33 @@ public class SettingsFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        // Set active url name
+        RPCUrl url = VariableHolder.getInstance().activeUrl();
+        selectedNetwork.setText(url.getName());
     }
 
     // Actions
 
     @OnClick(R.id.settings_select_network_view)
-    private void selectNetworkClicked() {
+    void selectNetworkClicked() {
+        SettingsSelectNetworkFragment selectNetworkFragment = new SettingsSelectNetworkFragment();
 
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.detach(this);
+        fragmentTransaction.setPrimaryNavigationFragment(selectNetworkFragment);
+        fragmentTransaction.add(R.id.navigation_content_view, selectNetworkFragment, "details");
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 
     @OnClick(R.id.settings_tracked_tokens_view)
-    private void trackedTokensClicked() {
+    void trackedTokensClicked() {
 
     }
 
     @OnClick(R.id.settings_change_password_view)
-    private void changePasswordClicked() {
+    void changePasswordClicked() {
 
     }
 }
